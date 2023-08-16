86cf59008b4fd6d5bcf1a70cbe54afb9a90ee1ae
HenSchutz <86529207+HenSchutz@users.noreply.github.com>
2023-08-15 22:08:04 -0300

    Revertendo - Faltou criar branch

--- main.py
+++ main.py
@@ -25,13 +25,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-def opcao_2():
-    print("Você escolheu a opção 2.")
-
-(dentro da função exibir_menu)
-print("2. Opção 2")
-
-(dentro do while)
-elif escolha == 2:
-            opcao_2()
-
